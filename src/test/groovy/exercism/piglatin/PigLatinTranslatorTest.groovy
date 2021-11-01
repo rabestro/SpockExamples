2ef85f5dccf088a459eea3fe48de96e6b705d8b3
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-01 19:02:48 +0200

    Add PigLatinTranslatorTest

--- src/test/groovy/exercism/piglatin/PigLatinTranslatorTest.groovy
+++ src/test/groovy/exercism/piglatin/PigLatinTranslatorTest.groovy
@@ -2,9 +2,11 @@ package exercism.piglatin
 
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class PigLatinTranslatorTest extends Specification {
-    def "Translate"() {
+
+    def "test #comment"() {
         given: "an instance of PigLatinTranslator"
         def translator = new PigLatinTranslator()
 
@@ -12,97 +14,28 @@ class PigLatinTranslatorTest extends Specification {
         translator.translate(phrase) == pigLatin
 
         where:
-        phrase   | pigLatin   | comment
-        'apple'  | 'appleay'  | ''
-        'ear'    | 'earay'    | ''
-        'igloo'  | 'iglooay'  | 'word beginning with i'
-        'object' | 'objectay' | 'word beginning with o'
-        'under'  | 'underay'  | 'word beginning with u'
+        phrase           | pigLatin               | comment
+        'apple'          | 'appleay'              | 'word beginning with a'
+        'ear'            | 'earay'                | 'word beginning with e'
+        'igloo'          | 'iglooay'              | 'word beginning with i'
+        'object'         | 'objectay'             | 'word beginning with o'
+        'under'          | 'underay'              | 'word beginning with u'
+        'equal'          | 'equalay'              | 'word beginning with vowel and followed by qu'
+        'pig'            | 'igpay'                | 'word beginning with p'
+        'koala'          | 'oalakay'              | 'word beginning with k'
+        'xenon'          | 'enonxay'              | 'word beginning with x'
+        'qat'            | 'atqay'                | 'word beginning with q without a following u'
+        'chair'          | 'airchay'              | 'ch treated like a consonant at the beginning of a word'
+        'queen'          | 'eenquay'              | 'qu treated like a consonant at the beginning of a word'
+        'square'         | 'aresquay'             | 'qu and a preceding consonant treated like a consonant at the beginning of a word'
+        'therapy'        | 'erapythay'            | 'th treated like a consonant at the beginning of a word'
+        'thrush'         | 'ushthray'             | 'thr treated like a consonant at the beginning of a word'
+        'school'         | 'oolschay'             | 'sch treated like a consonant at the beginning of a word'
+        'yttria'         | 'yttriaay'             | 'yt treated like a vowel at the beginning of a word'
+        'xray'           | 'xrayay'               | 'xr treated like a vowel at the beginning of a word'
+        'yellow'         | 'ellowyay'             | 'y treated like a consonant at the beginning of a word'
+        'rhythm'         | 'ythmrhay'             | 'y treated like a vowel at the end of a consonant cluster'
+        'my'             | 'ymay'                 | 'y as second letter in two letter word'
+        'quick fast run' | 'ickquay astfay unray' | 'a whole phrase'
     }
 }
-
-/*
-    public void testWordBeginningWithVowelAndFollowedByQu() {
-        assertEquals("equalay", pigLatinTranslator.translate("equal"));
-    }
-
-    @Test
-    public void testWordBeginningWithP() {
-        assertEquals("igpay", pigLatinTranslator.translate("pig"));
-    }
-
-    @Test
-    public void testWordBeginningWithK() {
-        assertEquals("oalakay", pigLatinTranslator.translate("koala"));
-    }
-
-    @Test
-    public void testWordBeginningWithX() {
-        assertEquals("enonxay", pigLatinTranslator.translate("xenon"));
-    }
-
-    @Test
-    public void testWordBeginningWithQWithoutAFollowingU() {
-        assertEquals("atqay", pigLatinTranslator.translate("qat"));
-    }
-
-    @Test
-    public void testChTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("airchay", pigLatinTranslator.translate("chair"));
-    }
-
-    @Test
-    public void testQuTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("eenquay", pigLatinTranslator.translate("queen"));
-    }
-
-    @Test
-    public void testQuAndAPrecedingConsonantTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("aresquay", pigLatinTranslator.translate("square"));
-    }
-
-    @Test
-    public void testThTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("erapythay", pigLatinTranslator.translate("therapy"));
-    }
-
-    @Test
-    public void testThrTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("ushthray", pigLatinTranslator.translate("thrush"));
-    }
-
-    @Test
-    public void testSchTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("oolschay", pigLatinTranslator.translate("school"));
-    }
-
-    @Test
-    public void testYtTreatedLikeAVowelAtTheBeginningOfAWord() {
-        assertEquals("yttriaay", pigLatinTranslator.translate("yttria"));
-    }
-
-    @Test
-    public void testXrTreatedLikeAVowelAtTheBeginningOfAWord() {
-        assertEquals("xrayay", pigLatinTranslator.translate("xray"));
-    }
-
-    @Test
-    public void testYTreatedLikeAConsonantAtTheBeginningOfAWord() {
-        assertEquals("ellowyay", pigLatinTranslator.translate("yellow"));
-    }
-
-    @Test
-    public void testYTreatedLikeAVowelAtTheEndOfAConsonantCluster() {
-        assertEquals("ythmrhay", pigLatinTranslator.translate("rhythm"));
-    }
-
-    @Test
-    public void testYAsSecondLetterInTwoLetterWord() {
-        assertEquals("ymay", pigLatinTranslator.translate("my"));
-    }
-
-    @Test
-    public void testAWholePhrase() {
-        assertEquals("ickquay astfay unray", pigLatinTranslator.translate("quick fast run"));
-    }
- */
